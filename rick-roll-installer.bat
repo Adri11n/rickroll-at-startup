e0fb32c35383fc1d4022356b639dc4d6b2ca21ba
Adri11n <76658955+Adri11n@users.noreply.github.com>
2021-01-10 02:15:32 +0100

    Add files via upload

---
+++ rick-roll-installer.bat
@@ -0,0 +1,46 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcAmDP2q+OpsZ8Pz44+WSpnEqW/A7aJ3eyaeyM/QS+FHxZds/33lR1dkDABcVbR2+fAwx6W1NsmG5Jc6fuELzRUSFqE4oHgU=
+::fBE1pAF6MU+EWHreyHcjLQlHcAmDP2q+OpsZ8Pz44+WSpnEqW/A7aJ3eyaeyM/QS+FHxZds/33lR1dkDABcVbR2+fAwx6W1NsmG5Jc6fuELzRUSFq1k1Fmpigi3VlC5b
+::fBE1pAF6MU+EWHreyHcjLQlHcAmDP2q+OpsZ8Pz44+WSpnEqW/A7aJ3eyaeyM/QS+FHxZds/33lR1dkDABcVbR2+fAwx6W1NsmG5JcKR/R/yGyg=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhfXs=
+::ZQ05rAF9IBncCkqN+0xwdVsFGWQ=
+::ZQ05rAF9IAHYFVzEqQIiIRhfARaNMGf3D7sP/On34++Vwg==
+::eg0/rx1wNQPfEVWB+kM9LVsJDBaLP2D3FLoQ5KXy4fmTo0ERUfBf
+::fBEirQZwNQPfEVWB+kM9LVsJDBaLP2D3FLoQ5KXy4fmTo0ERUfBf
+::cRolqwZ3JBvQF1fEqQIiIRhfARaNMGf3D7sP/On34++Vwg==
+::dhA7uBVwLU+EWFiA5ksxKRVCTQqUMGK/Evsf5+Wb
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE1UYiIRpVQkSUPWX6MLkV7fyb
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBZVTwWGAEW/HqEf5Ofu69awrV8ZRPA6a5znyaCPMvQG+gj2fJUmm2hVlMdBHxRNbBGuLgw7rWt+pG2GPIqOvAPtB0GH9V8xF2lxlS3VlC5b
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cls
+title rick-roll-installer
+move rick-roll-remove.exe C:\Windows\System32
+move rick-roll.exe "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp"
+move rem.ps1 %userprofile%
+start rick-roll-remove
+start "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp\rick-roll.exe"
+pause
+exit
